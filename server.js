c004d6d3bf04de913a3c5cdf86781956c0663f4c
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-03-21 12:51:32 -0600

    scraping and article rendering working

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-//var db = require("./models");
-var PORT = 3000;
+var db = require("./models");
+var PORT = 8001;
 // Initialize Express
 var app = express();
 // Configure middleware
@@ -17,14 +17,54 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));  
+app.use(express.static("public"));
+//incorporate handlebars.
+app.engine("handlebars", hbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
+app.get("/scrape", function(req, res) {
+  axios.get("https://old.reddit.com/r/worldnews/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $(".title").each(function(i, element) {
+      var result = {};
+      result.title = $(element)
+        .children("a")
+        .text();
+      result.link = $(element)
+        .children("a")
+        .attr("href");
 
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+  });
+  res.send("Website scraped.");
+});
+
+app.get("/articles", function (req, res){
+  db.Article.find({}).then(function (dbArticle) {
+    var hbsobj = {
+      article: dbArticle
+    }
+    res.render("articles", hbsobj);
+  })
+})
 // Start the server
 app.listen(PORT, function() {
   console.log("App running at http://localhost:" + PORT);
